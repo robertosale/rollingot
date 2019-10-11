0846e648d326054d1c868e65ca4690cd94def73e
Roberto Sale <robertito.sale@gmail.com>
2019-10-11 20:50:47 -0300

    first commit benja main

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+window.onload=function(){
+	const signUpButton = document.getElementById('signUp');
+	const signInButton = document.getElementById('signIn');
+	const container = document.getElementById('container-login');
+
+	signUpButton.addEventListener('click', () => {
+		container.classList.add("right-panel-active");
+	});
+
+	signInButton.addEventListener('click', () => {
+		container.classList.remove("right-panel-active");
+	});
+}
